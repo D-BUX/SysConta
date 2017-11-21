a3ffda3bbc2bf151446d90993e23326ee3eaaea7
Brandux <branduxjuarez@upeu.edu.pe>
2017-11-21 02:35:27 -0500

    sas

--- planillas/src/main/java/pe/edu/upeu/planilla/dao/EmpleadoDAO.java
+++ planillas/src/main/java/pe/edu/upeu/planilla/dao/EmpleadoDAO.java
@@ -18,7 +18,7 @@ public class EmpleadoDAO {
     public List<Map<String, Object>> listarempleados()
     {
        String sql="select upper(p.nombre) nombre,upper(p.apellido) apellido,upper(e.empleadocategoria)categoria,upper(c.cargo) cargo, upper(a.area) area,upper(d.departamentonombre) departamento,upper(em.empresanombre) empresa\r\n" + 
-       		"from planillasdb.persona p,planillasdb.empleado e,planillasdb.contrato co,planillasdb.cargo c,planillasdb.area a,planillasdb.departamento d,planillasdb.empresa em,planillasdb.sueldo s\r\n" + 
+       		"from planillasdb.persona p,planillasdb.empleado e,planillasdb.contrato co,planillasdb.cargo c, planillasdb.area a,planillasdb.departamento d,planillasdb.empresa em,planillasdb.sueldo s\r\n" + 
        		"where p.idpersona=e.idpersona and e.idempleado=co.idempleado and co.idcargo=c.idcargo and s.idsueldo=c.idsueldo and c.idarea=a.idarea and d.iddepartamento=a.iddepartamento and em.idempresa=d.idempresa;"; 
        return jt.queryForList(sql);
        
